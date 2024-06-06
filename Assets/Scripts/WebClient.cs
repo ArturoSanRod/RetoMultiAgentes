641c29b56ce53d9b974feb12fc601593a261b09a
Jesus Ramirez <105653438+j1rd2@users.noreply.github.com>
2024-06-06 10:11:43 -0600

    Proyect started

---
+++ Assets/Scripts/WebClient.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[System.Serializable]
+public class Dimensions
+{
+    public int n;
+    public int m;
+}
+
+[System.Serializable]
+public class InitialData
+{
+    public Dimensions dimensions;
+    public List<string[]> terrain;
+}
+
+public class WebClient : MonoBehaviour
+{
+    public GameObject groundPrefab;
+    public GameObject obstaclePrefab;
+    public GameObject trashPrefab;
+    public GameObject binPrefab;
+    public GameObject robotPrefab;
+
+    private void Start()
+    {
+        StartCoroutine(RequestInitialData());
+    }
+
+    IEnumerator RequestInitialData()
+    {
+        string url = "http://localhost:8585";
+        UnityWebRequest www = new UnityWebRequest(url, "POST");
+        www.downloadHandler = new DownloadHandlerBuffer();
+        www.uploadHandler = new UploadHandlerRaw(new byte[0]);
+        www.SetRequestHeader("Content-Type", "application/json");
+
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log(www.downloadHandler.text);
+            InitialData initialData = JsonUtility.FromJson<InitialData>(www.downloadHandler.text);
+            GenerateTerrain(initialData);
+        }
+    }
+
+    void GenerateTerrain(InitialData data)
+    {
+        for (int i = 0; i < data.dimensions.n; i++)
+        {
+            for (int j = 0; j < data.dimensions.m; j++)
+            {
+                Vector3 position = new Vector3(j, 0, -i);
+                string cell = data.terrain[i][j];
+
+                if (cell == "X")
+                {
+                    Instantiate(obstaclePrefab, position, Quaternion.identity);
+                }
+                else if (cell == "P")
+                {
+                    Instantiate(binPrefab, position, Quaternion.identity);
+                }
+                else if (cell != "0")
+                {
+                    GameObject trash = Instantiate(trashPrefab, position, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(groundPrefab, position, Quaternion.identity);
+                }
+            }
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Vector3 robotPosition = new Vector3(i, 0, 0);
+            Instantiate(robotPrefab, robotPosition, Quaternion.identity);
+        }
+    }
+}
